326a43d48be50c9e68ef8d013cd8517ed359f102
Calvin Adhikang <calvinadhikang@gmail.com>
2023-05-19 01:17:13 +0700

    Updated Database : Add Supplier Table

--- papicomfeed.sql
+++ papicomfeed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2023 at 03:41 PM
+-- Generation Time: May 18, 2023 at 08:16 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `papicomfeed`
 --
-CREATE DATABASE IF NOT EXISTS `papicomfeed` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `papicomfeed`;
 
 -- --------------------------------------------------------
 
@@ -42,8 +40,7 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`id`, `nama`, `alamat`, `telp`) VALUES
 (1, 'Customer 1', 'Surabaya Dekat ISTTS', 123123),
-(2, 'Customer 2', 'Surabaya dekat monumen pahlawan', 456456),
-(3, 'jojo', 'tpj', 813325325);
+(2, 'Customer 2', 'Surabaya dekat monumen pahlawan', 456456);
 
 -- --------------------------------------------------------
 
@@ -57,26 +54,16 @@ CREATE TABLE `dbeli` (
   `kolam_id` int(11) NOT NULL,
   `ikan_id` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  `nama_ikan` varchar(255) NOT NULL,
-  `tanggal` date NOT NULL DEFAULT current_timestamp(),
-  `subtotal` int(11) NOT NULL
+  `jumlah` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dbeli`
 --
 
-INSERT INTO `dbeli` (`id`, `hbeli_id`, `kolam_id`, `ikan_id`, `harga`, `jumlah`, `nama_ikan`, `tanggal`, `subtotal`) VALUES
-(9, 8, 1, 1, 1000, 2, 'Cupang Hias', '2023-05-01', 2000),
-(10, 9, 1, 3, 5000, 15, 'Gurami', '2023-05-02', 75000),
-(11, 9, 2, 2, 2500, 10, 'Lele', '1900-01-03', 25000),
-(12, 10, 1, 2, 2500, 5, 'Lele', '2023-05-14', 12500),
-(13, 10, 1, 1, 1000, 15, 'Cupang Hias', '2023-05-14', 15000),
-(14, 11, 1, 3, 5000, 2, 'Gurami', '2023-05-14', 10000),
-(15, 11, 1, 1, 1000, 5, 'Cupang Hias', '2023-05-14', 5000),
-(16, 12, 2, 2, 2500, 6, 'Lele', '2023-05-15', 15000),
-(17, 12, 2, 3, 5000, 7, 'Gurami', '2023-05-15', 35000);
+INSERT INTO `dbeli` (`id`, `hbeli_id`, `kolam_id`, `ikan_id`, `harga`, `jumlah`) VALUES
+(1, 1, 1, 3, 5000, 2),
+(2, 1, 1, 2, 2500, 2);
 
 -- --------------------------------------------------------
 
@@ -90,18 +77,16 @@ CREATE TABLE `djual` (
   `ikan_id` int(11) NOT NULL,
   `qty` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
-  `subtotal` int(11) NOT NULL,
-  `nama_ikan` varchar(255) NOT NULL,
-  `tanggal` date NOT NULL DEFAULT current_timestamp()
+  `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `djual`
 --
 
-INSERT INTO `djual` (`id`, `hjual_id`, `ikan_id`, `qty`, `harga`, `subtotal`, `nama_ikan`, `tanggal`) VALUES
-(9, 8, 1, 2, 1000, 2000, 'Cupang Hias', '2023-05-15'),
-(10, 8, 2, 4, 2500, 10000, 'Lele', '2023-05-15');
+INSERT INTO `djual` (`id`, `hjual_id`, `ikan_id`, `qty`, `harga`, `subtotal`) VALUES
+(1, 1, 3, 2, 100, 200),
+(2, 1, 2, 1, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -123,11 +108,7 @@ CREATE TABLE `hbeli` (
 --
 
 INSERT INTO `hbeli` (`id`, `karyawan_id`, `total`, `penjual`, `alamat`, `tanggal`) VALUES
-(8, 2, 2000, '', '', '2023-05-14 06:33:22.170189'),
-(9, 2, 100000, 'jojo', 'taman', '2023-05-14 06:33:59.057597'),
-(10, 2, 27500, 'ivander', 'simo', '2023-05-14 06:34:29.723788'),
-(11, 2, 15000, 'wina', 'pokcan', '2023-05-14 06:34:57.040008'),
-(12, 2, 50000, 'ed', 'manyar', '2023-05-15 01:23:09.595639');
+(1, 2, 15000, 'Supplier 1', 'Jalan 123123', '2023-04-06 04:23:16.414090');
 
 -- --------------------------------------------------------
 
@@ -149,7 +130,7 @@ CREATE TABLE `hjual` (
 --
 
 INSERT INTO `hjual` (`id`, `karyawan_id`, `total`, `customer`, `alamat`, `tanggal`) VALUES
-(8, 3, 12000, 1, 'Surabaya Dekat ISTTS', '2023-05-15 02:07:00.972245');
+(1, 3, 200, 1, 'alamat1', '2023-04-05 19:47:59.958261');
 
 -- --------------------------------------------------------
 
@@ -215,8 +196,27 @@ CREATE TABLE `kolam` (
 --
 
 INSERT INTO `kolam` (`id`, `nama`, `kapasitas`, `status`) VALUES
-(1, 'Kolam Utara 1', 100, 0),
-(2, 'kolam selatan', 70, 0);
+(1, 'Kolam Utara 1', 100, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `telp` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `nama`, `alamat`, `telp`) VALUES
+(1, 'Layar Supplier Corp', 'jalan cupang murah Gang 4', '567567');
 
 --
 -- Indexes for dumped tables
@@ -271,6 +271,12 @@ ALTER TABLE `kolam`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -278,31 +284,31 @@ ALTER TABLE `kolam`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `dbeli`
 --
 ALTER TABLE `dbeli`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `djual`
 --
 ALTER TABLE `djual`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `hbeli`
 --
 ALTER TABLE `hbeli`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `hjual`
 --
 ALTER TABLE `hjual`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `ikan`
@@ -320,7 +326,13 @@ ALTER TABLE `karyawan`
 -- AUTO_INCREMENT for table `kolam`
 --
 ALTER TABLE `kolam`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
